8e60a13aa08fb11b0e12892c4d92a3498e0185b7
ap-dominik.kocka <ap-dominik.kocka@PC-DEV-4>
2016-09-01 10:51:57 +0200

    added direct debit

--- new_domestic/TestClass.cs
+++ new_domestic/TestClass.cs
@@ -6,7 +6,6 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
-
 namespace SeleniumTests
 {
     [TestFixture]
@@ -16,8 +15,7 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
-        private int dom = 0, sep = 0, swi = 0, zus = 0, tax = 0;
-
+        private int dom = 0, sep = 0, swi = 0, zus = 0, tax = 0, dd = 0;
         [SetUp]
         public void SetupTest()
         {
@@ -25,7 +23,6 @@ namespace SeleniumTests
             baseURL = @"https://clicpltest.egroup.hu";
             verificationErrors = new StringBuilder();
         }
-
         [TearDown]
         public void TeardownTest()
         {
@@ -34,13 +31,9 @@ namespace SeleniumTests
                 driver.Quit();
             }
             catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
+            { }
             Assert.AreEqual("", verificationErrors.ToString());
         }
-
-        
         [Test]
         public void DomesticTest()
         {
@@ -55,7 +48,6 @@ namespace SeleniumTests
             }
             catch { }
             dom = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[1]/td[2]")).Text);
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
             driver.Navigate().GoToUrl(baseURL + "/Domestic/New");
             driver.FindElement(By.Id("Input_BnName-Search")).Click(); Thread.Sleep(3000);
             driver.FindElement(By.Id("gvDomesticPartnerSearch_tccell0_3")).Click(); Thread.Sleep(1000);
@@ -85,8 +77,6 @@ namespace SeleniumTests
             {
                 verificationErrors.Append(e.Message);
             }
-
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
         }
 
         [Test]
@@ -103,7 +93,6 @@ namespace SeleniumTests
             }
             catch { }
             sep = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[2]/td[2]")).Text);
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
             driver.Navigate().GoToUrl(baseURL + "/Sepa/New");
             driver.FindElement(By.Id("Input_BnName-Search")).Click(); Thread.Sleep(3000);
             driver.FindElement(By.Id("gvForeignPartnerSearch_DXDataRow17")).Click(); Thread.Sleep(1000);
@@ -111,7 +100,6 @@ namespace SeleniumTests
             driver.FindElement(By.Id("Input_Details")).SendKeys("automaticTest");
             driver.FindElement(By.Id("Input_ExtRef")).Clear();
             driver.FindElement(By.Id("Input_ExtRef")).SendKeys("automaticTest");
-            //new SelectElement(driver.FindElement(By.Id("Input_CountryId"))).SelectByText("GERMANY");
             driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
             driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -149,7 +137,6 @@ namespace SeleniumTests
             }
             catch { }
             swi = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[3]/td[2]")).Text);
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
             driver.Navigate().GoToUrl(baseURL + "/Swift/New");
             driver.FindElement(By.Id("Input_BnName-Search")).Click(); Thread.Sleep(3000);
             driver.FindElement(By.Id("gvForeignPartnerSearch_DXDataRow14")).Click(); Thread.Sleep(1000);
@@ -157,7 +144,6 @@ namespace SeleniumTests
             driver.FindElement(By.Id("Input_Details")).SendKeys("automaticTest");
             driver.FindElement(By.Id("Input_ExtRef")).Clear();
             driver.FindElement(By.Id("Input_ExtRef")).SendKeys("automaticTest");
-            //new SelectElement(driver.FindElement(By.Id("Input_CountryId"))).SelectByText("GERMANY");
             driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
             driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
             driver.FindElement(By.Id("actionButton_Save")).Click();
@@ -195,7 +181,6 @@ namespace SeleniumTests
             }
             catch { }
             zus = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[4]/td[2]")).Text);
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
             driver.Navigate().GoToUrl(baseURL + "/DomesticZus/New");
             driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
             driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
@@ -204,10 +189,6 @@ namespace SeleniumTests
             DateTime datum = DateTime.Today;
             datum.ToString().ToCharArray();
             char[] tomb = new char[6];
-            /*for (int i = 0; i < datum.ToString().ToCharArray().Length; i++)
-            {
-                driver.FindElement(By.Id("Input_PayerSupplementaryIdNumber")).SendKeys((datum.ToString().ToCharArray()[i]).ToString());
-            }*/
             int szam = 0;
             for (int i = 0; i < 8; i++)
             {
@@ -219,22 +200,9 @@ namespace SeleniumTests
                 driver.FindElement(By.Id(id)).SendKeys((datum.ToString().ToCharArray()[i]).ToString());
                 szam++;
             }
-
-
-
-            /*tomb = datum.ToString().Split(tomb.ToCharArray(),
-                                      StringSplitOptions.RemoveEmptyEntries);
-            object[] list = new object[tomb.Length];
-
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                list[i] = Convert.ChangeType(tomb[i], type);
-            {*/
             driver.FindElement(By.Id("Input_DeclarationNumber")).Clear();
             driver.FindElement(By.Id("Input_DeclarationNumber")).SendKeys("40");
-
             driver.FindElement(By.Id("Input_PayerSupplementaryIdNumber")).SendKeys("YXN083441");
-            
             driver.FindElement(By.Id("actionButton_Save")).Click();
             try
             {
@@ -256,9 +224,6 @@ namespace SeleniumTests
                 verificationErrors.Append(e.Message);
             }
         }
-
-        /* CANNOT BE DONE AT VERSION 1.1.1481.0 !!!
-         * (finished but save not working)*/
         [Test]
         public void TaxTest()
         {
@@ -273,21 +238,15 @@ namespace SeleniumTests
             }
             catch { }
             tax = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[5]/td[2]")).Text);
-            // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
             driver.Navigate().GoToUrl(baseURL + "/DomesticTax/New");
             driver.FindElement(By.Id("dk1-Input_BnAddressBnName")).Click();
             driver.FindElement(By.Id("dk1-Aleksandrów-Kujawski - Urząd Skarbowy")).Click();
             new SelectElement(driver.FindElement(By.Id("Input_BnAddressBnName"))).SelectByText("Aleksandrów Kujawski - Urząd Skarbowy");
             driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
             driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
-
-
-
             new SelectElement(driver.FindElement(By.Id("Input_BnAccount"))).SelectByText("42101010780024112227000000 - Other");
-            //driver.FindElement(By.CssSelector("option[value=\"42101010780024112227000000\"]")).Click();
             driver.FindElement(By.Id("Input_LiabilityIdentification")).Clear();
             driver.FindElement(By.Id("Input_LiabilityIdentification")).SendKeys("YXN083441");
-
             driver.FindElement(By.Id("actionButton_Save")).Click();
             try
             {
@@ -309,8 +268,50 @@ namespace SeleniumTests
                 verificationErrors.Append(e.Message);
             }
         }
-        /**/
-
+        [Test]
+        public void DirectDebitTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/Login/Login");
+            driver.FindElement(By.LinkText("Login with RSA token [DEMO]")).Click();
+            driver.FindElement(By.Id("loginId")).Clear();
+            driver.FindElement(By.Id("loginId")).SendKeys("100003");
+            driver.FindElement(By.Id("login")).Click();
+            try
+            {
+                driver.FindElement(By.Id("submit")).Click();
+            }
+            catch { }
+            dd = int.Parse(driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[7]/td[2]")).Text);
+            driver.Navigate().GoToUrl(baseURL + "/DirectDebit/New");
+            driver.FindElement(By.Id("Input_BnName-Search")).Click(); Thread.Sleep(3000);
+            driver.FindElement(By.Id("gvDirectDebitPartnerSearch_DXDataRow0")).Click(); Thread.Sleep(1000);
+            driver.FindElement(By.Id("Input_Details")).Clear();
+            driver.FindElement(By.Id("Input_Details")).SendKeys("automaticTest");
+            driver.FindElement(By.Id("Input_ExtRef")).Clear();
+            driver.FindElement(By.Id("Input_ExtRef")).SendKeys("automaticTest");
+            driver.FindElement(By.Id("Input_Amount_formatted")).Clear();
+            driver.FindElement(By.Id("Input_Amount_formatted")).SendKeys("1000,00");
+            driver.FindElement(By.Id("actionButton_Save")).Click();
+            try
+            {
+                driver.FindElement(By.Id("actionButton_Save")).Click();
+                Thread.Sleep(1000);
+                driver.Navigate().GoToUrl(baseURL + "/Home/Dashboard");
+            }
+            catch (Exception)
+            {
+                driver.Navigate().GoToUrl(baseURL + "/Home/Dashboard");
+            }
+            Thread.Sleep(1000);
+            try
+            {
+                Assert.AreEqual((dd + 1).ToString(), driver.FindElement(By.XPath(".//*[@id='main']/div[5]/div[2]/div/div/div/table/tbody/tr[7]/td[2]")).Text);
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+        }
         private bool IsElementPresent(By by)
         {
             try
@@ -323,7 +324,6 @@ namespace SeleniumTests
                 return false;
             }
         }
-
         private bool IsAlertPresent()
         {
             try
@@ -336,7 +336,6 @@ namespace SeleniumTests
                 return false;
             }
         }
-
         private string CloseAlertAndGetItsText()
         {
             try
